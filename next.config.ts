c2876d63f5338f47ee30183630628338bab666c8
Degbola <abeseyifunmi@gmail.com>
2026-03-19 16:24:07 +0100

    Mark jspdf/html2canvas as server external packages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ['jspdf', 'html2canvas', 'fflate'],
   headers: async () => [
     {
       source: '/(.*)',
